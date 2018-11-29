07962a66b8469d39ce61fab7648538be250ec1b7
MaryGracee <45455058+MaryGracee@users.noreply.github.com>
2018-11-29 12:24:06 +0100

    Update lezone1.rb

--- lezone1.rb
+++ lezone1.rb
@@ -7,3 +7,17 @@ live_loop :flibble do
   sample :bd_haus, rate: 1
   sleep 1
 end
+
+live_loop :guit do
+  with_fx :echo, mix: 0.3, phase: 0.25 do
+    sample :guit_em9, rate: 0.5
+  end
+  #  sample :guit_em9, rate: -0.5
+  sleep 8
+end
+live_loop :boom do
+  with_fx :reverb, room: 1 do
+    sample :bd_boom, amp: 10, rate: 1
+  end
+  sleep 8
+end
